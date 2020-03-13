c2a0365b27c3a84f8bf45fbf2b62ba4eec7815ca
Jeff Smith <toxicglados@gmail.com>
2020-03-13 16:25:47 -0500

    Moving computers

--- init.fish
+++ init.fish
@@ -8,16 +8,37 @@ if command -s pipenv > /dev/null
         if status --is-command-substitution
             return
         end
-        if not test -e "$PWD/Pipfile"
+        # If the PIPENV_MAX_DEPTH variable is defined and the pipenv shell is not active
+        if test -n "$PIPENV_MAX_DEPTH" && not test -n "$PIPENV_ACTIVE"
+            set -l dir $PWD
+            for i in (seq "$PIPENV_MAX_DEPTH")
+                echo "Checking $dir/ for Pipfile"
+                if test -e "$dir/Pipfile"
+                    set -l parent_found 1
+                    echo "Pipfile found at $dir"
+                    break
+                else
+                    set dir (dirname "$dir")
+                end
+            end
+        else
+            echo 'Else'
+        end
+
+
+
+        if not test -n "$parent_found"
             if not string match -q "$__pipenv_fish_initial_pwd"/'*' "$PWD/"
                 set -U __pipenv_fish_final_pwd "$PWD"
                 exit
             end
             return
         end
-
+        
         if not test -n "$PIPENV_ACTIVE"
+          echo 'boop'
           if pipenv --venv >/dev/null 2>&1
+            echo 'snoop'
             set -x __pipenv_fish_initial_pwd "$PWD"
 
             if [ "$pipenv_fish_fancy" = 'yes' ]
